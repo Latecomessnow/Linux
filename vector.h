b77864650ecd46921b21a6f8480173c19d825398
Latecomessnow <hui.yumhappy@gmail.com>
2022-09-12 22:52:50 +0800

    Add files via upload

--- vector.h
+++ vector.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include<assert.h>
+#include<iostream>
+#include <string.h>
 
 //namespace HuiGu
 //{
@@ -85,44 +87,178 @@ namespace HuiGu
 			delete[] _start;
 			_start = _finish = _end_of_storage = nullptr;
 		}
-
-		vector(cosnt vector<T>& v)
-		{
-			int sz = size();
-			T* tmp = new T[sz + 1];
-			memcpy(tmp, v._start, sizeof(T) * sz);
-			delete[] _start;
-			_start = tmp;
-			_finish = _end_of_storage = v._finish;
-		}
-
+		
 		size_t size() const
 		{
 			return _finish - _start;
 		}
 
-		size_t capacity() cosnt
+		size_t capacity() const
 		{
-			return _end_of_storage;
+			return _end_of_storage - _start;
 		}
 
-		vloid push_back(const T& x)
+		T& operator[](size_t pos)
 		{
+			assert(pos < size());
+			return *(_start + pos);
+		}
+
+		const T& operator[](size_t pos) const
+		{
+			assert(pos < size());
+			return *(_start + pos);
+		}
+
+		void reserve(size_t n)
+		{
+			if (n > capacity())
+			{
+				size_t sz = size();
+				T* tmp = new T[n];
+				// 如果原对象中存在数据，则需要拷贝数据
+				if (_start)
+				{
+					memcpy(tmp, _start, sizeof(T) * sz);
+					delete[] _start;
+				}
+				// 更新对象的信息
+				_start = tmp;
+				_finish = _start + sz;
+				_end_of_storage = _start + n;
+			}
+		}
+
+		//void resize(size_t n, const T& val = T())
+		//{
+		//	if (n > size())
+		//	{
+		//		reserve(n);
+		//	}
+		//	size_t sz = size();
+		//	for (size_t i = sz; i < capacity(); i++)
+		//	{
+		//		_start[i] = val;
+		//	}
+		//}
+
+		void push_back(const T& val)
+		{
+			//if (_finish == _end_of_storage)
+			//{
+			//	reserve(capacity() == 0 ? 4 : capacity() * 2);
+			//}
+			//*_finish = val;
+			//_finish++;
+			insert(end(), val);
+		}
+
+		void pop_back()
+		{
+			assert(_finish > _start);
+			_finish--;
+		}
+
+		// 使用迭代器表示pos位置
+		void insert(iterator pos, const T& x)
+		{
+			assert(pos >= _start);
+			assert(pos <= _finish);
+			// 插入空间可能不够，扩容，但要知道扩容后pos位置已经发生变化
 			if (_finish == _end_of_storage)
 			{
-				reserve(capacity() == 0 ? 4 : capacity * 2);
+				// 记录pos位置与_start的偏移量
+				size_t len = pos - _start;
+				reserve(capacity() == 0 ? 4 : capacity() * 2);
+				// 此时_start已表示新空间，更新新空间后的pos位置
+				pos = _start + len;
 			}
-			_finish = x;
+			// 挪动数据
+			iterator end = _finish - 1;
+			while (end >= pos)
+			{
+				*(end + 1) = *(end);
+				end--;
+			}
+			*pos = x;
 			_finish++;
 		}
 
-		void test_vector1()
+		// stl 规定erase返回删除位置下一个位置迭代器
+		iterator erase(iterator pos)
 		{
-			vector v1;
+			assert(pos >= _start);
+			assert(pos < _finish);
+			// pos + 1用后边的元素覆盖删除前边的数
+			iterator begin = pos + 1;
+			while (begin < _finish)
+			{
+				*(begin - 1) = *begin;
+				begin++;
+			}
+			_finish--;
+			return pos;
 		}
+
 	private:
 		iterator _start;
 		iterator _finish;
 		iterator _end_of_storage;
 	};
+	void test_vector1()
+	{
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		v.push_back(3);
+		v.push_back(4);
+		v.push_back(5);
+		v.push_back(6);
+		vector<int>::iterator it = v.begin();
+		while (it != v.end())
+		{
+			std::cout << *it << " ";
+			it++;
+		}
+		std::cout << std::endl;
+		v.pop_back();
+		v.pop_back();
+		v.pop_back();
+	}
+
+	void test_vector2()
+	{
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		v.push_back(3);
+		v.push_back(4);
+		v.push_back(5);
+		v.push_back(6);
+		v.push_back(7);
+		for (auto e : v)
+		{
+			std::cout << e << " ";
+		}
+		std::cout << std::endl;
+		vector<int>::iterator it = v.begin();
+		while (it != v.end())
+		{
+			if (*it % 2 == 0)
+			{
+				// 返回下一位置迭代器
+				it = v.erase(it);
+			}
+			// 不删除++下一位置
+			else
+			{
+				it++;
+			}
+		}
+		for (auto e : v)
+		{
+			std::cout << e << " ";
+		}
+		std::cout << std::endl;
+	}
 }
